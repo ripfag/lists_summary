d3bacec9d3d3c57bfa788a8529635061ae795ec8
ripfag <aleksandrbistrov1@bk.ru>
2026-02-28 19:01:36 +0300

    Update lists_sum.py

--- lists_sum.py
+++ lists_sum.py
@@ -1,7 +1,14 @@
-list1 = [1, 2, 3, 4, 5, 6]
-list2 = [7, 8, 9, 10, 11, 12]
-lists_summary = []
-for number in range(list1.__len__()):
-    lists_summary.append(list1[number]+list2[number])
+def lists_sum():
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    list2 = [7, 8, 9, 10, 11, 12, 13, 14, 15]
+    lists_summary = []
+    biggest_list = max(len(list1), len(list2))
 
-print(lists_summary)
+    for number in range(biggest_list):
+        num1 = list1[number] if number < len(list1) else 0
+        num2 = list2[number] if number < len(list2) else 0
+        lists_summary.append(num1 + num2)
+    print(lists_summary)
+
+
+lists_sum()
